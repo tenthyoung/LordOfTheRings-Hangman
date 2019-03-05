9cc7986daff4d21728c5ac22fd3fa26f160999a2
tenthyoung <tenthyoung@gmail.com>
2019-03-05 12:50:55 -0800

    added

---
+++ assets/javascript/pseudo.js
@@ -0,0 +1,13 @@
+//I need words
+//I need to validate user's input
+//if user's words are capital letters, then i need to add it
+//I need a function that takes the user input, and checks to 
+//see if it matches. 
+while(answer.indexOf("letter") === -1 ) {
+    //keep checking
+}
+//I need a function that kills a LOTR character if you get it 
+//wrong. 
+//I need a function that adds the letter if its right
+//I need a function that prints a victory when you win
+//I need a function prints a victory when you lose
